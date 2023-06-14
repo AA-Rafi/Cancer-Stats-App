57f2ff42bbd2cea34caf49a784eb3242f64ea226
AA-Rafi <124394388+AA-Rafi@users.noreply.github.com>
2023-06-14 00:46:31 -0400

    js fixes

---
+++ Datasets/clean_inci.js
@@ -0,0 +1,295 @@
+ [
+  {
+    id: "0",
+    REF_DATE: "1992",
+    VALUE: "117990"
+  },
+  {
+    id: "1",
+    REF_DATE: "1993",
+    VALUE: "121910"
+  },
+  {
+    id: "2",
+    REF_DATE: "1994",
+    VALUE: "122610"
+  },
+  {
+    id: "3",
+    REF_DATE: "1995",
+    VALUE: "122075"
+  },
+  {
+    id: "4",
+    REF_DATE: "1996",
+    VALUE: "123155"
+  },
+  {
+    id: "5",
+    REF_DATE: "1997",
+    VALUE: "126965"
+  },
+  {
+    id: "6",
+    REF_DATE: "1998",
+    VALUE: "131075"
+  },
+  {
+    id: "7",
+    REF_DATE: "1999",
+    VALUE: "135510"
+  },
+  {
+    id: "8",
+    REF_DATE: "2000",
+    VALUE: "138945"
+  },
+  {
+    id: "9",
+    REF_DATE: "2001",
+    VALUE: "142880"
+  },
+  {
+    id: "10",
+    REF_DATE: "2002",
+    VALUE: "145155"
+  },
+  {
+    id: "11",
+    REF_DATE: "2003",
+    VALUE: "147300"
+  },
+  {
+    id: "12",
+    REF_DATE: "2004",
+    VALUE: "152405"
+  },
+  {
+    id: "13",
+    REF_DATE: "2005",
+    VALUE: "156695"
+  },
+  {
+    id: "14",
+    REF_DATE: "2006",
+    VALUE: "161535"
+  },
+  {
+    id: "15",
+    REF_DATE: "2007",
+    VALUE: "167195"
+  },
+  {
+    id: "16",
+    REF_DATE: "2008",
+    VALUE: "168450"
+  },
+  {
+    id: "17",
+    REF_DATE: "2009",
+    VALUE: "173270"
+  },
+  {
+    id: "18",
+    REF_DATE: "2010",
+    VALUE: "187695"
+  },
+  {
+    id: "19",
+    REF_DATE: "2011",
+    VALUE: "195710"
+  },
+  {
+    id: "20",
+    REF_DATE: "2012",
+    VALUE: "197545"
+  },
+  {
+    id: "21",
+    REF_DATE: "2013",
+    VALUE: "193445"
+  },
+  {
+    id: "22",
+    REF_DATE: "2014",
+    VALUE: "198215"
+  },
+  {
+    id: "23",
+    REF_DATE: "2015",
+    VALUE: "201965"
+  },
+  {
+    id: "24",
+    REF_DATE: "2016",
+    VALUE: "207190"
+  },
+  {
+    id: "25",
+    REF_DATE: "2017",
+    VALUE: "211290"
+  },
+  {
+    id: "26",
+    REF_DATE: "2018",
+    VALUE: ""
+  },
+  {
+    id: "27",
+    REF_DATE: "2019",
+    VALUE: ""
+  },
+  {
+    id: "28",
+    REF_DATE: "2020",
+    VALUE: ""
+  },
+  {
+    id: "29",
+    REF_DATE: "1992",
+    VALUE: "415.9"
+  },
+  {
+    id: "30",
+    REF_DATE: "1993",
+    VALUE: "425"
+  },
+  {
+    id: "31",
+    REF_DATE: "1994",
+    VALUE: "422.8"
+  },
+  {
+    id: "32",
+    REF_DATE: "1995",
+    VALUE: "416.6"
+  },
+  {
+    id: "33",
+    REF_DATE: "1996",
+    VALUE: "415.9"
+  },
+  {
+    id: "34",
+    REF_DATE: "1997",
+    VALUE: "424.5"
+  },
+  {
+    id: "35",
+    REF_DATE: "1998",
+    VALUE: "434.7"
+  },
+  {
+    id: "36",
+    REF_DATE: "1999",
+    VALUE: "445.7"
+  },
+  {
+    id: "37",
+    REF_DATE: "2000",
+    VALUE: "452.8"
+  },
+  {
+    id: "38",
+    REF_DATE: "2001",
+    VALUE: "460.6"
+  },
+  {
+    id: "39",
+    REF_DATE: "2002",
+    VALUE: "462.9"
+  },
+  {
+    id: "40",
+    REF_DATE: "2003",
+    VALUE: "465.5"
+  },
+  {
+    id: "41",
+    REF_DATE: "2004",
+    VALUE: "477.2"
+  },
+  {
+    id: "42",
+    REF_DATE: "2005",
+    VALUE: "486"
+  },
+  {
+    id: "43",
+    REF_DATE: "2006",
+    VALUE: "495.9"
+  },
+  {
+    id: "44",
+    REF_DATE: "2007",
+    VALUE: "508.4"
+  },
+  {
+    id: "45",
+    REF_DATE: "2008",
+    VALUE: "506.7"
+  },
+  {
+    id: "46",
+    REF_DATE: "2009",
+    VALUE: "515.2"
+  },
+  {
+    id: "47",
+    REF_DATE: "2010",
+    VALUE: "552"
+  },
+  {
+    id: "48",
+    REF_DATE: "2011",
+    VALUE: "569.9"
+  },
+  {
+    id: "49",
+    REF_DATE: "2012",
+    VALUE: "569.1"
+  },
+  {
+    id: "50",
+    REF_DATE: "2013",
+    VALUE: "551.4"
+  },
+  {
+    id: "51",
+    REF_DATE: "2014",
+    VALUE: "559.3"
+  },
+  {
+    id: "52",
+    REF_DATE: "2015",
+    VALUE: "565.7"
+  },
+  {
+    id: "53",
+    REF_DATE: "2016",
+    VALUE: "573.8"
+  },
+  {
+    id: "54",
+    REF_DATE: "2017",
+    VALUE: "578.2"
+  },
+  {
+    id: "55",
+    REF_DATE: "2018",
+    VALUE: ""
+  },
+  {
+    id: "56",
+    REF_DATE: "2019",
+    VALUE: ""
+  },
+  {
+    id: "57",
+    REF_DATE: "2020",
+    VALUE: ""
+  },
+  {
+    id: ""
+  }
+]
