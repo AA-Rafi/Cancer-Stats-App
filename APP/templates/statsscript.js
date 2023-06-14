95da09e868462e1c02a99ade300a21fef7904539
EleshiaH <halleleshia@gmail.com>
2023-06-14 19:22:25 -0400

    adding pages

---
+++ APP/templates/statsscript.js
@@ -0,0 +1,132 @@
+clean_inci = [
+    {
+      id: "0",
+      REF_DATE: "1992",
+      VALUE: "117990"
+    },
+    {
+      id: "1",
+      REF_DATE: "1993",
+      VALUE: "121910"
+    },
+    {
+      id: "2",
+      REF_DATE: "1994",
+      VALUE: "122610"
+    },
+    {
+      id: "3",
+      REF_DATE: "1995",
+      VALUE: "122075"
+    },
+    {
+      id: "4",
+      REF_DATE: "1996",
+      VALUE: "123155"
+    },
+    {
+      id: "5",
+      REF_DATE: "1997",
+      VALUE: "126965"
+    },
+    {
+      id: "6",
+      REF_DATE: "1998",
+      VALUE: "131075"
+    },
+    {
+      id: "7",
+      REF_DATE: "1999",
+      VALUE: "135510"
+    },
+    {
+      id: "8",
+      REF_DATE: "2000",
+      VALUE: "138945"
+    },
+    {
+      id: "9",
+      REF_DATE: "2001",
+      VALUE: "142880"
+    },
+    {
+      id: "10",
+      REF_DATE: "2002",
+      VALUE: "145155"
+    },
+    {
+      id: "11",
+      REF_DATE: "2003",
+      VALUE: "147300"
+    },
+    {
+      id: "12",
+      REF_DATE: "2004",
+      VALUE: "152405"
+    },
+    {
+      id: "13",
+      REF_DATE: "2005",
+      VALUE: "156695"
+    },
+    {
+      id: "14",
+      REF_DATE: "2006",
+      VALUE: "161535"
+    },
+    {
+      id: "15",
+      REF_DATE: "2007",
+      VALUE: "167195"
+    },
+    {
+      id: "16",
+      REF_DATE: "2008",
+      VALUE: "168450"
+    },
+    {
+      id: "17",
+      REF_DATE: "2009",
+      VALUE: "173270"
+    },
+    {
+      id: "18",
+      REF_DATE: "2010",
+      VALUE: "187695"
+    },
+    {
+      id: "19",
+      REF_DATE: "2011",
+      VALUE: "195710"
+    },
+    {
+      id: "20",
+      REF_DATE: "2012",
+      VALUE: "197545"
+    },
+    {
+      id: "21",
+      REF_DATE: "2013",
+      VALUE: "193445"
+    },
+    {
+      id: "22",
+      REF_DATE: "2014",
+      VALUE: "198215"
+    },
+    {
+      id: "23",
+      REF_DATE: "2015",
+      VALUE: "201965"
+    },
+    {
+      id: "24",
+      REF_DATE: "2016",
+      VALUE: "207190"
+    },
+    {
+      id: "25",
+      REF_DATE: "2017",
+      VALUE: "211290"
+    }
+  ]
