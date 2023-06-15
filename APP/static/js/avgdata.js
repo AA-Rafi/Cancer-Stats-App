346ca7c598f55c2a4978efa19b2bf2ac488045d9
EleshiaH <halleleshia@gmail.com>
2023-06-15 12:41:05 -0400

    added the rates to the html

---
+++ APP/static/js/avgdata.js
@@ -0,0 +1,54 @@
+avgdata = [
+  {
+    Province: "Newfoundland and Labrador",
+    VALUE: "9.821"
+  },
+  {
+    Province: "Prince Edward Island",
+    VALUE: "10.722"
+  },
+  {
+    Province: "Nova Scotia",
+    VALUE: "10.258"
+  },
+  {
+    Province: "New Brunswick",
+    VALUE: "10.617"
+  },
+  {
+    Province: "Ontario",
+    VALUE: "9.309"
+  },
+  {
+    Province: "Quebec",
+    VALUE: "9.564"
+  },
+  {
+    Province: "Manitoba",
+    VALUE: "9.458"
+  },
+  {
+    Province: "Saskatchewan",
+    VALUE: "9.278"
+  },
+  {
+    Province: "Alberta",
+    VALUE: "7.712"
+  },
+  {
+    Province: "British Columbia",
+    VALUE: "9.239"
+  },
+  {
+    Province: "Yukon",
+    VALUE: "6.446"
+  },
+  {
+    Province: "Northwest Territories",
+    VALUE: "5.393"
+  },
+  {
+    Province: "Nunavut",
+    VALUE: "3.414"
+  },
+]
